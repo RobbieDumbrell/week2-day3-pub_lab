f687b4300efcb8103f3cf815cafaa8d4b8304396
Robbie Dumbrell <robert.dumbrell@outlook.com>
2018-07-04 11:28:21 +0100

    customer class created and getter tests passed

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -4,6 +4,16 @@ require_relative('../customer.rb')
 
 class TestCusomter < MiniTest::Test
 
+  def setup
+    @mark = Customer.new("mark",10)
+  end
 
+  def test_check_customer_name
+    assert_equal("mark", @mark.name)
+  end
+  
+  def test_check_customer_wallet
+    assert_equal(10, @mark.wallet)
+  end
 
 end
